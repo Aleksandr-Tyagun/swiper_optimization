2652238b499b8983230c54a400d6727232e52f07
aleksandr-tyagun <alexander.tiagun@gmail.com>
2020-08-12 12:13:30 +0300

    only works component

--- src/scenes/HomePage/HomePage.js
+++ src/scenes/HomePage/HomePage.js
@@ -1,34 +1,34 @@
 import React from 'react';
-import Projects from '@scenes/Projects';
-import MailUs from '@scenes/MailUs';
-import Hero from './components/Hero';
-import WhatWeDo from './components/WhatWeDo';
-import Testimonials from './components/Testimonials';
-import Gallery from './components/Gallery';
-import Posts from './components/Posts';
+// import Projects from '@scenes/Projects';
+// import MailUs from '@scenes/MailUs';
+// import Hero from './components/Hero';
+// import WhatWeDo from './components/WhatWeDo';
+// import Testimonials from './components/Testimonials';
+// import Gallery from './components/Gallery';
+// import Posts from './components/Posts';
 import Works from './components/Works';
 
 import PropTypes from 'prop-types';
 
 import styles from './HomePage.module.scss';
 
-const HomePage = ({ animation }) => {
+const HomePage = () => {
   return (
     <div className={styles.container}>
       <div className="pageWrapper">
-        <Hero animation={animation} />
-        <WhatWeDo />
+        {/* <Hero animation={animation} />
+        <WhatWeDo /> */}
       </div>
       <Works />
-      <div className="pageWrapper">
+      {/* <div className="pageWrapper">
         <Projects title="Projects" navigation={true} />
-      </div>
-      <Testimonials />
+      </div> */}
+      {/* <Testimonials />
       <Gallery />
       <div className="pageWrapper">
         <Posts />
       </div>
-      <MailUs />
+      <MailUs /> */}
     </div>
   );
 };
